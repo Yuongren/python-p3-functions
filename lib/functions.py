bb4a5222cf57e305f69ddce0f52d200b307dbf97
gitYoungren <youngrengitonga@gmail.com>
2024-11-26 12:13:00 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print(f"Hello, programmer!")
     pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 +num2
+result = add(45, 55)
+print(result)
+
 
 def halve(number):
-    pass
+    result = number/2
+    return result
+    print(result)
